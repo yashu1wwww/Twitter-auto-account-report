c0a16209936ff0acfbe0ddd60668343b2e835f82
Yashu_Krish11 <89150253+yashu1wwww@users.noreply.github.com>
2023-01-06 07:38:38 +0530

    Update t.py

--- t.py
+++ t.py
@@ -7,19 +7,17 @@ from selenium.webdriver.support import expected_conditions as EC
 
 driver= webdriver.Chrome()
 driver.maximize_window()
-
 driver.get("https://twitter.com/i/flow/login")
-time.sleep(10)
+time.sleep(5)
 email = driver.find_element_by_name('text')
-email.send_keys("123-3t") #replace with your twitter account 
+email.send_keys("123_yoy") #replace with your twitter account 
 email.send_keys(Keys.ENTER)
 time.sleep(3)
 password = driver.find_element_by_name("password") 
-password.send_keys("seen23%")#replace with your twitter password 
+password.send_keys("pass_find")#replace with your twitter password 
 password.send_keys(Keys.ENTER)
-time.sleep(8)
-
-driver.get("https://twitter.com/Botter_11") #replace wityh username
+time.sleep(4)
+driver.get('https://twitter.com/Botter_11') #replace with username which you want to report that account after .com/
 time.sleep(3)
 driver.find_element_by_xpath('/html/body/div[1]/div/div/div[2]/main/div/div/div/div/div/div[3]/div/div/div/div/div[1]/div[2]/div[1]/div').click()#id click
 time.sleep(3)
@@ -27,11 +25,11 @@ driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/di
 time.sleep(3)
 driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div').click()#start report
 time.sleep(3)
-driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[2]/div/div/label[1]').click()#who is these report for
+driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[2]/div/label[1]/div[1]/div').click() #who is these report for
 time.sleep(3)
 driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div/div/div').click()#next button
 time.sleep(3)
-driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[2]/div/div/label[1]').click()#what is these happening to them
+driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[2]/div/label[1]/div[1]/div[1]').click()#what is these happening to them
 time.sleep(3)
 driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div/div/div/div').click()#next button
 time.sleep(3)
@@ -43,8 +41,8 @@ driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/di
 time.sleep(3)
 driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div/div/div[1]/div').click()#next
 time.sleep(3)
-driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[2]/div/div/label[1]').click()#last question where is these happening for profile
-#driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[2]/div/div/label[2]').click()#for tweets
+driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[2]/div/label[1]/div[1]/div[1]').click()#last question where is these happening for profile
+#driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[2]/div/label[2]/div[1]/div').click()#for tweets
 time.sleep(3)
 driver.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div/div/div/div').click()#next button
 time.sleep(3)
